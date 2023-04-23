1eee7e4ff8b77d749f8b558eb9cb25a32cb6b5d1
shnabbydoodle <thelahshow@gmail.com>
2023-04-23 18:23:06 -0400

    good golly that's a lot of homepage stuff!

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
+/** old parallaxing stuff, irrelevant now (4/23/23 6pm)
 $(document).ready(function(){
     $(window).scroll(function(){
         var scrollTop = $(window).scrollTop();
         $('.video-container').css('top', -(scrollTop * 0.2) + 'px');
     });
 });
+ */
